083ad792aea0bf0194795bf3219fbf74a05d3824
Alex1505Gold <sgolenkov2002@gmail.com>
2021-05-03 20:59:43 +0300

    Complete task

---
+++ solver_application/_build/CMakeFiles/formatter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/formatter.dir/root/Alex1505Gold/workspace/projects/timp_lab03/formatter_lib/formatter.cpp.o"
+  "libformatter.pdb"
+  "libformatter.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/formatter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
